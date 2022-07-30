6c6c142f2fbc4238232c4b1596e30cc1fe64f06f
Benjamin <benjamin.guy.thomas@gmail.com>
2022-07-30 17:20:58 +0200

    Create/update async (rendering HTML is blocking otherwise)

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,6 @@
 class Quote < ApplicationRecord
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
   after_destroy_commit -> { broadcast_remove_to "quotes" }
 
   validates :name, presence: true
