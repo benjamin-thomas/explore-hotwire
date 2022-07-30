d6ebc9af6acf55b93a56129665ecba5ec6adecb5
Benjamin <benjamin.guy.thomas@gmail.com>
2022-07-30 17:25:51 +0200

    Use shortcut!

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,5 @@
 class Quote < ApplicationRecord
-  after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  after_update_commit -> { broadcast_replace_later_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  broadcasts_to ->(_) { "quotes" }, inserts_by: :prepend
 
   validates :name, presence: true
 
