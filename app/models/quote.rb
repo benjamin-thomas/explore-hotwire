9a3c484757e7355bb7bf8c70e8a34d76ec237749
Benjamin <benjamin.guy.thomas@gmail.com>
2022-08-03 05:48:13 +0200

    Annotate

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: quotes
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  company_id :bigint           not null
+#
+# Indexes
+#
+#  index_quotes_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
 class Quote < ApplicationRecord
   belongs_to :company
   has_many :bundles, dependent: :destroy
--- test/models/bundle_test.rb
+++ test/models/bundle_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: bundles
+#
+#  id         :bigint           not null, primary key
+#  ship_on    :date             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  quote_id   :bigint           not null
+#
+# Indexes
+#
+#  index_bundles_on_quote_id              (quote_id)
+#  index_bundles_on_quote_id_and_ship_on  (quote_id,ship_on) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (quote_id => quotes.id)
+#
 require "test_helper"
 
 class BundleTest < ActiveSupport::TestCase
